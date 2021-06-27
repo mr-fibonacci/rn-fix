5ec7eaa4a04c14702a7ca5ee9918e30bd0e82788
mr-fibonacci <adamlapinski1@gmail.com>
2021-06-27 15:20:51 +0000

    added remaining filters, 14 total

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,6 +12,11 @@ import { Inkwell } from "../filters/Inkwell";
 import { Kelvin } from "../filters/Kelvin";
 import { Lofi } from "../filters/Lofi";
 import { Nashville } from "../filters/Nashville";
+import { Rise } from "../filters/Rise";
+import { Toaster } from "../filters/Toaster";
+import { Valencia } from "../filters/Valencia";
+import { Walden } from "../filters/Walden";
+import { Xpro2 } from "../filters/Xpro2";
 
 export default function HomeScreen(props) {
   const { message } = props;
@@ -82,6 +87,41 @@ export default function HomeScreen(props) {
               uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
             }}
           </Nashville>
+        </Surface>
+        <Surface style={{ width: 300, height: 300 }}>
+          <Rise>
+            {{
+              uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
+            }}
+          </Rise>
+        </Surface>
+        <Surface style={{ width: 300, height: 300 }}>
+          <Toaster>
+            {{
+              uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
+            }}
+          </Toaster>
+        </Surface>
+        <Surface style={{ width: 300, height: 300 }}>
+          <Valencia>
+            {{
+              uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
+            }}
+          </Valencia>
+        </Surface>
+        <Surface style={{ width: 300, height: 300 }}>
+          <Walden>
+            {{
+              uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
+            }}
+          </Walden>
+        </Surface>
+        <Surface style={{ width: 300, height: 300 }}>
+          <Xpro2>
+            {{
+              uri: "https://res.cloudinary.com/drlqahj5d/image/upload/v1/media/images/hamburg_feh1uf.jpg",
+            }}
+          </Xpro2>
         </Surface>
       </ScrollView>
     </SafeAreaView>
